b7d3e17114ffc7b74839c5b5fa241357d6197fcc
mehdi077 <mehdi@mehdis-mbp.mynetworksettings.com>
2025-12-04 15:52:20 -0500

    Update

--- components/TiptapEditor.tsx
+++ components/TiptapEditor.tsx
@@ -119,8 +119,8 @@ const TiptapEditor = ({ initialContent, onContentUpdate }: TiptapEditorProps) =>
   }, []);
 
   // Get effective values (mobile uses hardcoded tight values)
-  const effectiveLineHeight = isMobile ? 0.5 : lineHeight;
-  const effectiveHorizontalPadding = isMobile ? 0.5 : horizontalPadding;
+  const effectiveLineHeight = isMobile ? 1.2 : lineHeight;
+  const effectiveHorizontalPadding = isMobile ? 1 : horizontalPadding;
 
   // Update editor styles when controls change
   useEffect(() => {
